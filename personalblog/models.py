6040768fb62e4d69f7b30a84c2e807195ebb1cd9
Nuell-A <alcala.arredondo1@gmail.com>
2023-01-01 19:11:12 -0800

    Created models, views, templates, and more

--- personalblog/models.py
+++ personalblog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+# User being the author of the post.
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    # When an author is deleted, then so will all the posts from them.
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+
+    def __str__(self):
+        # A query will result in the post title and author
+        return self.title + ' | ' + str(self.author)
--- personalblog/urls.py
+++ personalblog/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from . import views
+#from . import views
+from .views import HomeView, PostDetailView
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    #path('', views.home, name='home'),
+    path('', HomeView.as_view(), name='home'),
+
+    # <int:pk> being the primary key of each post (created automatically)
+    path('posts/<int:pk>', PostDetailView.as_view(), name='post_details'),
 ]
